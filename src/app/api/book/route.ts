1812e100a9cad4e8766b75cea71d0871ed454366
Mariam-Aamir <ummeareesh691@gmail.com>
2024-11-15 18:13:36 +0500

    first commit

---
+++ src/app/api/book/route.ts
@@ -0,0 +1,30 @@
+import { NextResponse } from "next/server";
+
+
+// export function GET(request:NextRequest){
+//    return NextResponse.json({messege:"my first API"}) 
+
+// }
+
+
+let books =  [
+    {id:1, title:"Harry Potter", author:"J.K Rowling", available:true },
+    {id:2, title:"Raja Gidh", author:"Bano Qudsia", available:true },
+    {id:3, title:"Bange Dara", author:"Allama Iqbal", available:true },
+    {id:4, title:"Mushaf", author:"Umaira Ahmed", available:true },
+
+]
+export async function GET(){
+    try{
+        return NextResponse.json(books, {status:200})
+
+    } catch(error){
+        return NextResponse.json(
+            {
+                message: "Error fetching books"},
+                 {status:500}
+        )
+    }
+}
+
+
